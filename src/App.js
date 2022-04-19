0ad85f254840637b029872232effce2527fc7a45
Marcus <marcus@pop-os.localdomain>
2022-04-19 14:14:33 +1000

    My Countdown Clock, first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import logo from './Logo.png';
 import './App.css';
+import CountdownTimer from "./CountdownTimer";
 
+const Time =  {
+  years: 10,
+  months: 0,
+  days: 3,
+  hours: 2,
+  mins: 8,
+  seconds: 42
+};
+
+  
 function App() {
+  const ten_Years_MS = 10 * 52 * 7 * 24 * 60 * 60 * 1000;
+  const NOW_MS = new Date().getTime(); 
+
+  const dateTimeAfterTenYears = ten_Years_MS + NOW_MS;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="Hello" />
+        <h1>
+          Marcus's Climate Counter 
+        </h1>
+        <CountdownTimer targetDate={dateTimeAfterTenYears}/>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/marcus-rehbock-52673b159/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Created by Marcus Rehbock (19/04/22)
         </a>
+
       </header>
     </div>
   );
